b0552fc8b931194b75692ae97674c333a24519b3
Luca Casola <lcasola@myseneca.ca>
2025-11-01 14:42:32 -0400

    Remove unused metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+// src/app/layout.tsx
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -18,8 +20,6 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "IT Resolved",
   description: "Your go-to IT solutions",
-  themeColor: '#000000',
-  viewport: 'width=device-width, initial-scale=1'
 };
 
 export default function RootLayout({
