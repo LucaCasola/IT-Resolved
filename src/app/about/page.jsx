37ba55d2fc4a76afe8030fdbc4d77974c8cbd5d1
Luca Casola <lcasola@myseneca.ca>
2025-11-01 14:42:04 -0400

    Change "about" page file extension from jsx -> tsx

--- src/app/about/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-  return (
-    <div className="flex align-center justify-center pt-5">
-      <h1>not active</h1>
-    </div>
-  );
-}
---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+// src/app/about/page.tsx
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About",
+  description: "Learn more about IT Resolved",
+};
+
+export default function About() {
+  return (
+    <div className="flex align-center justify-center pt-5">
+      <h1>not active</h1>
+    </div>
+  );
+}
