13c3058c39889593ee46b0dd11ec8893c9f4e5d8
Luca Casola <lucacasola0@gmail.com>
2025-11-08 14:03:04 -0500

    Styling

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -6,7 +6,7 @@ import { AspectRatio } from "@radix-ui/react-aspect-ratio";
 export default function HeroBanner() {
   return (
     <section className="relative">
-      <div className="absolute z-8 inset-0 bg-gray-700 opacity-40 rounded-md"></div>
+      <div className="absolute z-8 inset-0 bg-gray-700 opacity-40"></div>
       <div className="absolute z-9 inset-0 flex items-center justify-center">
         <h1 className="text-white">Your IT Issues Resolved</h1>
       </div>
