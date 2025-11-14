ac1e43890b9280da619230fbd60b7c1d1ae86026
Luca Casola <lucacasola0@gmail.com>
2025-11-14 12:51:06 -0500

    Create Office Support page

---
+++ src/app/services/office-support/page.tsx
@@ -0,0 +1,21 @@
+// src/app/services/office-support/page.tsx
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Office Support Services | IT Resolved",
+  description: "Information about Office Support services offered by IT Resolved",
+};
+
+export default function OfficeSupport() {
+  return (
+    <main>
+      <div className="col">
+        <h1>Office Support</h1>
+        <p className="page-description">
+          A
+        </p>
+      </div>
+    </main>
+  );
+}
