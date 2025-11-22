041c7197404426d1c491346f8775b52f09b309df
Luca Casola <lucacasola0@gmail.com>
2025-11-22 15:53:57 -0500

    Expose BASE_URL since it is not a secret

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.BASE_URL;
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
   return [
     {
