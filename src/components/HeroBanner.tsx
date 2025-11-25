6da0e74da377220d0bd545953b31863daca1fed9
Luca Casola <lucacasola0@gmail.com>
2025-11-25 15:41:12 -0500

    Add arrow in HeroBanner that takes user to first section on homepage
    close #39

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -1,13 +1,33 @@
 // src/components/HeroBanner.tsx
 
+"use client"
+
 import Image from "next/image"
+
+// UI component from shadcn/ui
 import { AspectRatio } from "@radix-ui/react-aspect-ratio";
+
+import { ChevronDownIcon } from "lucide-react";  // Icon
 
 export default function HeroBanner() {
   return (
     <section className="relative">
       <div className="absolute z-8 inset-0 bg-gray-700 opacity-40"></div>
-      <h2 className="absolute z-9 inset-0 flex items-center justify-center text-white font-bold italic text-center text-3xl md:text-4xl lg:text-4xl xl:text-6xl">Your IT Issues Resolved</h2>
+      <div className="absolute inset-0 z-9 col justify-center">
+        <h2 className="text-white m-0! font-bold italic text-3xl md:text-4xl lg:text-4xl xl:text-6xl">Your IT Issues Resolved</h2>
+        <ChevronDownIcon 
+          className="size-8 md:size-12 lg:size-12 xl:size-18 hover:transition-transform duration-300 hover:translate-y-3" 
+          aria-hidden="true" 
+          color="white" 
+          strokeWidth={2.4}
+          onClick={() => {
+            var element = document.getElementById("scrollto");
+            if (element) {
+              element.scrollIntoView({block: "start", behavior: 'smooth'});
+            }
+          }}
+        />
+      </div>
       <AspectRatio ratio={16 / 7}>
         <Image
           src="/images/it-resolved-hero.jpg"
